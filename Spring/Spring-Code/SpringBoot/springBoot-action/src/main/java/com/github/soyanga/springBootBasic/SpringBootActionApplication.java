87c192b3589705f27b5ea1c87c88b2df8a073ff5
SOYANGA <321830735@qq.com>
2019-05-05 17:49:32 +0800

    Spring-Code-SpringBoot-SpringBoot实战代码完结 Spring基础学习完结

--- Spring/Spring-Code/SpringBoot/springBoot-action/src/main/java/com/github/soyanga/springBootBasic/SpringBootActionApplication.java
+++ Spring/Spring-Code/SpringBoot/springBoot-action/src/main/java/com/github/soyanga/springBootBasic/SpringBootActionApplication.java
@@ -3,6 +3,8 @@ package com.github.soyanga.springBootBasic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ImportResource;
 
 
@@ -14,9 +16,26 @@ import org.springframework.context.annotation.ImportResource;
 @SpringBootApplication()
 
 @ImportResource(locations = {"classpath:application-bean.xml"})
-public class SpringBootActionApplication {
+public class SpringBootActionApplication extends SpringBootServletInitializer {
+
+    //Tomcat中会调用该方法启动
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringBootActionApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(SpringBootActionApplication.class, args);
     }
 }
+
+
+//@SpringBootApplication()
+//
+//@ImportResource(locations = {"classpath:application-bean.xml"})
+//public class SpringBootActionApplication {
+//
+//    public static void main(String[] args) {
+//        SpringApplication.run(SpringBootActionApplication.class, args);
+//    }
+//}
